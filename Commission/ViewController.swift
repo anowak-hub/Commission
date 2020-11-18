379a2230e7b537d377f690ebedb7e9bc75429fcf
Andrew Nowak <anowak4320@stu.d214.org>
2020-11-18 15:22:23 -0600

    Added various stack views and labels. Also added multiple IBActions and an IBOutlet.

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var commissionPayTextField: UITextField!
+    @IBOutlet weak var totalPayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func whenCalculateButtonPressed(_ sender: Any) {
+    }
+    
 }
 
