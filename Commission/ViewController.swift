ebf6af7bd91abe96decbc7286d979dc95739f5da
Andrew Nowak <anowak4320@stu.d214.org>
2020-11-18 14:37:28 -0600

    Modified the Storyboard to show iPhone 11 Pro Max. Also added a comment which says "GitHub Testing."

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -5,14 +5,14 @@
 //  Created by Andrew Nowak on 11/18/20.
 //  Copyright © 2020 Andrew Nowak. All rights reserved.
 //
-
+// GitHub Testing
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
